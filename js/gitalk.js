caee654d625ab61cfb1b5cad870f7cc583761b21
994300880@qq.com <994300880@qq.com>
2019-04-10 12:06:15 +0800

    Site updated: 2019-04-10 12:05:55

--- js/gitalk.js
+++ js/gitalk.js
@@ -1,11 +1,11 @@
-var gitalk = new Gitalk({
-  clientID: 'GitHub Application Client ID',
-  clientSecret: 'GitHub Application Client Secret',
-  repo: 'GitHub repo',
-  owner: 'GitHub repo owner',
-  admin: ['GitHub repo owner and collaborators, only these guys can initialize github issues'],
-  id: location.pathname,      // Ensure uniqueness and length less than 50
-  distractionFreeMode: false  // Facebook-like distraction free mode
-})
+const gitalk = new Gitalk({
+  clientID: '79365aa30e12daa029aa',
+  clientSecret: 'd1c8ec5600c61118a2048f5af562e6e7d32e0c9c',
+  repo: 'qq994300880.github.io',
+  owner: 'qq994300880',
+  admin: ['qq994300880'],
+  //id: location.pathname,     // Ensure uniqueness and length less than 50
+  distractionFreeMode: true  // Facebook-like distraction free mode
+});
 
-gitalk.render('gitalk-container')
+gitalk.render('gitalk-container');
